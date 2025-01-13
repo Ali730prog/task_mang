4f68c11a7b594143b6ec94f23bd270953e150c40
Ali730prog <alihsnian799@gmail.com>
2025-01-13 18:16:24 +0500

    Last update of the auth screeen

--- lib/common_widgets/light_text.dart
+++ lib/common_widgets/light_text.dart
@@ -11,9 +11,10 @@ class LightText extends StatefulWidget {
   const LightText(
       {super.key,
       required this.text,
-      this.textcolor=AppColor.whiteFFFFFF,
-      this.textsize=12,
-      this.weight=FontWeight.w500});
+      this.textcolor = AppColor.whiteFFFFFF,
+      this.textsize = 12,
+      this.weight = FontWeight.w500,
+      });
 
   @override
   State<LightText> createState() => _LightTextState();
@@ -23,6 +24,7 @@ class _LightTextState extends State<LightText> {
   @override
   Widget build(BuildContext context) {
     return Text(
+
       widget.text,
       style: GoogleFonts.inter(
         fontWeight: widget.weight,
