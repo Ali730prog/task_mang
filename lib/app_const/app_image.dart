dd11404a9ba648870ef0372a9415c25f20867f20
Ali730prog <alihsnian799@gmail.com>
2025-01-13 14:43:52 +0500

    Hello

---
+++ lib/app_const/app_image.dart
@@ -0,0 +1,16 @@
+class AppImage{
+
+  static const String _baseUrl = 'assets/image/';
+
+  static const String splahImage = "${_baseUrl}splah_image.png";
+  static const String app_logo = "${_baseUrl}app_logo.png";
+  static const String google_icon = "${_baseUrl}google_icon.png";
+  static const String avatar_1 = "${_baseUrl}avatar_1.png";
+  static const String clender = "${_baseUrl}clender.png";
+  static const String group = "${_baseUrl}group.png";
+  static const String avatar_2 = "${_baseUrl}avatar_2.png";
+  static const String avatar_3 = "${_baseUrl}avatar_3.png";
+  static const String avatar_4 = "${_baseUrl}avatar_4.png";
+
+
+}
