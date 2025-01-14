ddd67dd788badf0ff967c0758475564b1b66bb3b
salman10001234 <m.salmanmunir725626@gmail.com>
2025-01-13 16:17:38 -0800

    bold text

--- lib/common_widgets/bold_text.dart
+++ lib/common_widgets/bold_text.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:task_manage_ment/app_const/app_color.dart';
 
 class BoldText extends StatefulWidget {
-  // fina
-  const BoldText({super.key});
+  final String text;
+  final double? textsize;
+  final Color? textcolor;
+  final FontWeight? weight;
+  const BoldText(
+      {super.key,
+      required this.text,
+      this.textsize=26.0,
+      this.textcolor=AppColor.whiteFFFFFF,
+      this.weight=FontWeight.w600});
 
   @override
   State<BoldText> createState() => _BoldTextState();
@@ -12,11 +21,13 @@ class BoldText extends StatefulWidget {
 class _BoldTextState extends State<BoldText> {
   @override
   Widget build(BuildContext context) {
-    return Text("bold Text",style: GoogleFonts.inter(
-      fontWeight: FontWeight.w600,
-      fontSize: 26,
-      color: Colors.white,
-
-    ),);
+    return Text(
+      widget.text,
+      style: GoogleFonts.inter(
+        fontWeight: widget.weight,
+        fontSize:widget.textsize,
+        color: widget.textcolor,
+      ),
+    );
   }
 }
